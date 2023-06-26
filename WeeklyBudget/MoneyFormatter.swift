4b97c708ac270e545fe30314b2d3f327e9bc23ba
lukmansp <58159683+lukmansp@users.noreply.github.com>
2023-06-26 14:27:17 +0700

    first initialization

---
+++ WeeklyBudget/MoneyFormatter.swift
@@ -0,0 +1,12 @@
+//
+//  MoneyFormatter.swift
+//  WeeklyBudget
+//
+//  Created by lukmanma on 05/06/23.
+//
+
+import Foundation
+
+struct MoneyFormatter {
+    
+}
---
+++ WeeklyBudgetBase/MoneyFormatterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyFormatterTests.swift
+//  WeeklyBudgetTests
+//
+//  Created by lukmanma on 05/06/23.
+//
+
+import XCTest
+@testable import WeeklyBudget
+
+final class MoneyFormatterTests: XCTestCase {
+
+    func testMoneyFormatterWholeNumber(){
+        let formatter = MoneyFormatter()
+    }
+
+}
